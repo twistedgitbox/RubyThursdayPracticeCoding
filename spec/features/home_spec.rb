69012ea64644f1e022144e9e2964d3ee73cf8320
Michael K <jcisecurebuildinfo@gmail.com>
2016-02-06 21:45:22 +0000

    Added Home Controller and Root Path with spec

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature "visiting the homepage" do
   scenario "the visitor sees welcome text" do
     visit root_path
-    expect(page).to_have_text("Welcome to RubyThursday")
+    expect(page).to have_text("Welcome to RubyThursday")
   end
 
 end
